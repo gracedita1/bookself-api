1911473dc4842c567d7ff40cbb4eb6d34fab77e9
gracedita1 <gracedita1@gmail.com>
2022-04-18 12:27:51 +0700

    initialize project

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const { addBookHandler } = require("./handler");
+const routes = [
+  {
+    method: "POST",
+    path: "/books",
+    handler: addBookHandler,
+  },
+  //   {
+  //     method: "GET",
+  //     path: "/books/{id}",
+  //     handler: () => {},
+  //   },
+  //   {
+  //     method: "GET",
+  //     path: "/books",
+  //     handler: () => {},
+  //   },
+  //   {
+  //     method: "DELETE",
+  //     path: "/books/{id}",
+  //     handler: () => {},
+  //   },
+  //   {
+  //     method: "PUT",
+  //     path: "/books/{id}",
+  //     handler: () => {},
+  //   },
+];
+module.exports = routes;
